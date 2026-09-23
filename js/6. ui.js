25e6b5aad4ae6713493e82e9a5a21c15bd9a6b00
Leo-0107 <linm25280@gmail.com>
2026-09-23 15:24:33 +0900

    Reduce 3D waveform movement

--- js/6. ui.js
+++ js/6. ui.js
@@ -456,7 +456,7 @@
         const centerX = width * 0.50;
         const nearHalfWidth = width * 0.43;
         const farHalfWidth = width * 0.13;
-        const maxHeight = height * 2.20;
+        const maxHeight = height * 1.70;
 
         if (rows.length) {
           for (let t = rows.length - 1; t >= 0; t--) {
