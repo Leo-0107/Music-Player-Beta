6d8cf7a4e23a04695360e774b6522dab7cb8e084
Leo-0107 <linm25280@gmail.com>
2026-09-25 20:36:50 +0900

    Restore playback after invalid channel node settings

--- js/2. audio.js
+++ js/2. audio.js
@@ -1,4 +1,4 @@
-  const BUILD_REVISION = 25;
+  const BUILD_REVISION = 26;
 
   const titleObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -271,13 +271,9 @@
       }
 
       channelSplitter = audioCtx.createChannelSplitter(2);
-      channelSplitter.channelCountMode = "explicit";
-      channelSplitter.channelCount = 2;
-      channelSplitter.channelInterpretation = "speakers";
       leftGainNode = audioCtx.createGain();
       rightGainNode = audioCtx.createGain();
       channelMerger = audioCtx.createChannelMerger(2);
-      channelMerger.channelInterpretation = "speakers";
       leftGainNode.gain.value = currentLeftVolumeTarget;
       rightGainNode.gain.value = currentRightVolumeTarget;
 
